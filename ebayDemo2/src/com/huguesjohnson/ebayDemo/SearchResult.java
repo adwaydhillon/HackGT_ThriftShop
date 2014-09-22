c0f102340131780f2aec2d468dd04929464df58c
srijansood <Srijan@lawn-128-61-24-221.lawn.gatech.edu>
2014-09-21 22:06:15 -0400

    Final Commit for Submission

---
+++ ebayDemo2/src/com/huguesjohnson/ebayDemo/SearchResult.java
@@ -0,0 +1,45 @@
+package com.huguesjohnson.ebayDemo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SearchResult{
+	public final static int RESULT_SUCCESS=0;
+	public final static int RESULT_ERROR=1;
+	private int resultCode;
+	private Exception error;
+	private ArrayList<Listing> listings;
+
+	public SearchResult(){
+		this.listings=new ArrayList<Listing>();
+	}
+	
+	public void append(SearchResult toAppend){
+		this.listings.addAll(toAppend.getListings());
+	}
+	
+	public Exception getError(){
+		return error;
+	}
+	public void setError(Exception error){
+		this.error=error;
+	}
+	public ArrayList<Listing> getListings(){
+		return listings;
+	}
+	public void setListings(ArrayList<Listing> listings){
+		this.listings=listings;
+	}
+
+	public int getResultCode(){
+		return resultCode;
+	}
+
+	public void setResultCode(int resultCode){
+		this.resultCode=resultCode;
+	}
+	
+	public void sort(){
+		Collections.sort(this.listings);
+	}
+}
